ea92a062bbd28395f69b4ad47a3ecfcb0bdd428b
penzulo <bhardesh2005@gmail.com>
2025-11-26 01:31:43 +0530

    feat(strings): Add `strtoken` libc implementation.
    Added re-entrant version of `strtoken` based on the
    `strspan` and `strcspan` functions.

--- challenges/string_cspan.c
+++ challenges/string_cspan.c
@@ -1,14 +1,14 @@
 /**
- * Program: Custom `strspan` function
- * ----------------------------------
- * Create a `strspn` function which is similar to the
- * standard libraries `strspn` function. Optimization
- * is optional for now.
+ * Program: Custom 'strcspan' function
+ * -----------------------------------
+ * Create a `strcspan` function which is similar to the
+ * standard library `strcspn` function. Optimization is
+ * optional for now.
  */
 #include <assert.h>
+#include <stdint.h>
 #include <stdio.h>
 
-#define size_t unsigned long
 #define MODE_SET 0
 #define MODE_CHECK 1
 #define BITS (8 * sizeof(size_t))
@@ -18,7 +18,7 @@
  * But using this function makes the bitwise operations much
  * clearer to understand.
  */
-int bitop(size_t *bitmap, unsigned char c, unsigned short mode) {
+int bitop(size_t *bitmap, uint8_t c, uint8_t mode) {
   size_t index = c / BITS;
   size_t offset = c % BITS;
   size_t mask = (size_t)1 << offset;
@@ -28,9 +28,6 @@ int bitop(size_t *bitmap, unsigned char c, unsigned short mode) {
     return 1;
   }
 
-  /* NOTE: `!!` is supposed to normalize the return
-   * value of the bitwise operation between 0 and 1.
-   */
   return !!(bitmap[index] & mask);
 }
 
@@ -38,10 +35,10 @@ int bitop(size_t *bitmap, unsigned char c, unsigned short mode) {
  * @brief Get length of a substring
  *
  * Calculates the length (in bytes) of the initial segment
- * of @p space which consists entirely of bytes in @p accept.
+ * of @p space which entirely of bytes NOT in @p reject.
  *
- * @param[in] accept
- *      Acceptable array of characters.
+ * @param[in] reject
+ *      Rejectable array of characters.
  *
  * @param[in] space
  *      String to perform the function on.
@@ -50,35 +47,35 @@ int bitop(size_t *bitmap, unsigned char c, unsigned short mode) {
  * - Difference in index between the first character not in
  *   @p accept and the start of the substring.
  */
-size_t strspan(const char *accept, const char *space) {
+size_t strcspan(const char *reject, const char *space) {
   size_t bitmap[32 / sizeof(size_t)] = {0};
   const char *counter = space;
 
-  for (; *accept && bitop(bitmap, *accept, MODE_SET); accept++) {};
-  for (; *counter && bitop(bitmap, *counter, MODE_CHECK); counter++) {};
+  for (; *reject && bitop(bitmap, *reject, MODE_SET); reject++) {};
+  for (; *counter && !bitop(bitmap, *counter, MODE_CHECK); counter++) {};
 
   return counter - space;
 }
 
-void test_string_span() {
-  const char *strspace = "aaabbbcccxyz";
-  const char *acceptable = "abc";
+void test_string_cspan() {
+  const char *strspace = "Bhargav;;";
+  const char *rejectable = ";";
 
-  assert(strspan(acceptable, strspace) == 9);
+  assert(strcspan(rejectable, strspace) == 7);
   puts("Test 1 Passed: Valid substring length found.");
 }
 
-void test_string_span_empty() {
+void test_string_cspan_empty() {
   const char *strspace = "aaaabbbb";
-  const char *acceptable = "";
+  const char *rejectable = "";
 
-  assert(strspan(acceptable, strspace) == 0);
+  assert(strcspan(rejectable, strspace) == 8);
   puts("Test 2 Passed: Empty substring length found.");
 }
 
 void run_all_tests() {
-  test_string_span();
-  test_string_span_empty();
+  test_string_cspan();
+  test_string_cspan_empty();
 }
 
 int main() {
